2ace1c3a9849edb61e5b69e29fa3798e0d220931
HoangPC98 <git@github.com:HoangPC98>
2024-12-10 00:26:40 +0700

    done service RabbitMQ

--- src/services/RabbitMQService.ts
+++ src/services/RabbitMQService.ts
@@ -4,6 +4,9 @@ import config from "../config/config";
 class RabbitMQService {
   private requestQueue = "MSQ_REQUEST";
   private responseQueue = "MSQ_RESPONSE";
+  private sendSMSQueue = 'QUEUE_SEND_SMS';
+  private sendEmailQueue = 'QUEUE_SEND_MAIL';
+  private pushNotificationQueue = 'QUEUE_PUSH_NOTIF';
   private connection!: Connection;
   private channel!: Channel;
 
@@ -13,35 +16,75 @@ class RabbitMQService {
 
   async init() {
     // Establish connection to RabbitMQ server
-    this.connection = await amqp.connect(config.msgBrokerURL!);
+    this.connection = await amqp.connect(config.msgBrokerURL);
     this.channel = await this.connection.createChannel();
-
-    // Asserting queues ensures they exist
-    await this.channel.assertQueue(this.requestQueue, { durable: true });
-    await this.channel.assertQueue(this.responseQueue, { durable: true });
-
+    this.channel.assertExchange('1234', 'direct', {
+      durable: false
+    });
     // Start listening for messages on the request queue
-    this.listenForRequests();
+    // this.consumeQueueBName(this.sendSMSQueue);
+    // this.consumeQueueBName(this.sendEmailQueue);
+    // this.consumeQueueBName(this.pushNotificationQueue);
+    this.consumeSMS();
+    this.consumeEmail();
+    this.consumeNotification();
+    this.consumeNotificationByKey();
   }
 
-  private async listenForRequests() {
-    this.channel.consume(this.requestQueue, async (msg) => {
-      if (msg && msg.content) {
-        console.log('MS...received', msg)
-        // Send the user details response
-        this.channel.sendToQueue(
-          this.responseQueue,
-          Buffer.from(JSON.stringify({ msg: msg })),
-          {
-            correlationId: msg.properties.correlationId,
-            persistent: true  // make sure msg won't be lost even RMQ restart (RMQ will save msg to disk in advanced)
-          }
-        );
-        console.log(" [x] Sent %s", msg);
+  async consumeQueueBName(queueName: string) {
+    await this.channel.assertQueue(queueName, { durable: true });
+    this.channel.consume(queueName, async (msg) => {
+      console.log('MESSAGE 11', msg?.content.toString())
+      if (msg) {
         // Acknowledge the processed message
         this.channel.ack(msg);
       }
     });
+  }
+
+  async consumeSMS() {
+    await this.channel.assertQueue(this.sendSMSQueue, { durable: true });
+    this.channel.consume(this.sendSMSQueue, async (msg) => {
+      if (msg) {
+        console.log('QUEUE_SMS...', msg?.content.toString())
+        // Acknowledge the processed message
+        this.channel.ack(msg);
+      }
+    })
+  }
+
+  async consumeEmail() {
+    await this.channel.assertQueue(this.sendEmailQueue, { durable: true });
+    this.channel.consume(this.sendEmailQueue, async (msg) => {
+      if (msg) {
+        console.log('QUEUE_EMAIL...', msg?.content.toString())
+        // Acknowledge the processed message
+        this.channel.ack(msg);
+      }
+    })
+  }
+
+  async consumeNotification() {
+    await this.channel.assertQueue(this.pushNotificationQueue, { durable: true });
+    this.channel.consume(this.pushNotificationQueue, async (msg) => {
+      if (msg) {
+        console.log('QUEUE_NOTIFICATION...', msg?.content.toString())
+        // Acknowledge the processed message
+        this.channel.ack(msg);
+      }
+    })
+  }
+
+  async consumeNotificationByKey() {
+    await this.channel.assertQueue('', { durable: true });
+    await this.channel.bindQueue('', '1234', 'hoangpc'),
+    this.channel.consume('', async (msg) => {
+      if (msg) {
+        console.log('QUEUE_NONE...', msg?.content.toString().toString())
+        // Acknowledge the processed message
+        this.channel.ack(msg);
+      }
+    })
   }
 }
 
